97ad44ce3024e275f53251557d50fde1decfc39a
BhaneshvarKshirsagar <bhaneshvarkshirsagar0207@gmail.com>
2024-01-12 09:41:16 +0530

    start learning

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         This is my first React project
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Follow for more
         </a>
       </header>
     </div>
